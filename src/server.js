ec751e013bc8bb4948cc0ac3d02372484bc4ca61
nathan <grimaudathan@gmail.com>
2018-01-22 20:30:51 +0100

    wip

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ export default function start(){
     const port = process.env['API_PORT'] || 3000
     const server = new Server({
         host: '0.0.0.0',
-        port: 3000
+        port: 3000,
+        routes: {cors: {origin: ['*']} }
     })
     const db = open('amiral.db')
     db.then((database)=>{
