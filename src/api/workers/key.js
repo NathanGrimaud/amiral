8923a70f7e73220bb06a9f5d5eb7bfa9003c54b3
nathan <grimaudathan@gmail.com>
2018-01-23 15:59:40 +0100

    fix(timer)

--- src/api/workers/key.js
+++ src/api/workers/key.js
@@ -3,11 +3,11 @@ import {Observable} from 'rxjs'
 function generateKey(){
     
 }
-const hours = 60
-const minutes = 60
 const seconds = 1000
+const minutes = 60 * seconds
+const hours = 60 * minutes
 
-const defaultTimer = 20 * seconds
+const defaultTimer =  5 * minutes
 export default function ({KeysHandler},period = defaultTimer){
     return Observable.interval(defaultTimer)
         .startWith(0)
